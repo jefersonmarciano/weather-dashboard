a3cc1a53bc70f36bbb9781bc8a7b1170f7b4e27f
Jeferson marciano <44215511+jefersonmarciano@users.noreply.github.com>
2024-09-26 23:21:29 -0300

    converção de calculos

--- src/components/WeatherDetails.js
+++ src/components/WeatherDetails.js
@@ -5,6 +5,11 @@ import { useTranslation } from 'react-i18next';
 function WeatherDetails({ data }) {
   const { t } = useTranslation();
 
+  // Funções de conversão
+  const hPaToAtm = (hPa) => (hPa / 1013.25).toFixed(3);
+  const mpsToKmh = (mps) => (mps * 3.6).toFixed(1);
+  const mToKm = (m) => (m / 1000).toFixed(2);
+
   return (
     <Card sx={{ marginTop: 2 }}>
       <CardContent>
@@ -14,18 +19,18 @@ function WeatherDetails({ data }) {
         <Grid container spacing={2}>
           <Grid item xs={6}>
             <Typography variant="body1">
-              <strong>{t('humidity')}:</strong> {data.main.humidity}%
+              <strong>{t('Humidade')}:</strong> {data.main.humidity}%
             </Typography>
             <Typography variant="body1">
-              <strong>{t('pressure')}:</strong> {data.main.pressure} hPa
+              <strong>{t('Pressão')}:</strong> {hPaToAtm(data.main.pressure)} atm
             </Typography>
           </Grid>
           <Grid item xs={6}>
             <Typography variant="body1">
-              <strong>{t('wind')}:</strong> {data.wind.speed} m/s
+              <strong>{t('Vento')}:</strong> {mpsToKmh(data.wind.speed)} km/h
             </Typography>
             <Typography variant="body1">
-              <strong>{t('visibility')}:</strong> {data.visibility} m
+              <strong>{t('Visibilidade')}:</strong> {mToKm(data.visibility)} km
             </Typography>
           </Grid>
         </Grid>
